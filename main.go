67e65810f5fd4678f16752f146b9ef411057dc54
Bruno Marques <blmarques.dev@gmail.com>
2023-02-09 00:48:50 -0300

    feat(delivery): delivered challinger

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import (
+	"github.com/blmarquess/dio-go-numerics/parte1"
+	"github.com/blmarquess/dio-go-numerics/parte2"
+)
+
+func main() {
+	go parte1.PartOne()
+	go parte2.PinPan()
+}
